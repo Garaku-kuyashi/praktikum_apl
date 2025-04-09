afa7987d8f374c6dac89ec73f6cba58c33e31e09
Garaku-kuyashi <moraxz112@gmail.com>
2025-04-10 00:25:32 +0800

    finish posttest 4

---
+++ kelas/pertemuan-4/pertemuan-4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+// string arr[3] = {"a","b","c"};
+
+// void array (){
+//     string arr[3] = {"a","b","c"};
+// }
+
+
+
+// int luas (int p, int l){
+//     return p*l;
+// }
+
+// int luas (int s){
+//     return s*s;
+
+// }
+
+// int faktor (int n) {
+//     if (n == 1 || n == 0){
+//         return 1;
+//     }else {
+//         return n*faktor(n-1);
+//     }
+// }
+
+
+
+
+
+int main(){
+
+    // cout << luas(5,7) << endl;
+    // cout << luas(5) << endl;
+    // cout << faktor(3);
+
+
+    return 0;
+}
