e5cdd74b2b31bbf57b2efc4f10b9190708a746bd
Garaku-kuyashi <moraxz112@gmail.com>
2025-04-22 10:01:21 +0800

    finish posttest 5

---
+++ kelas/pertemuan-5/pertemuan-5.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+using namespace std;
+
+
+void angka (int arrayAp[100]){
+    
+}
+
+
+
+int main(){
+    cout << "helo" << endl;
+
+
+
+    int arrrayA [100] = {1,2,3};
+
+    
+    // operator * dan &
+    // * -> menginisialisasi variabel pointer dan menagkases value
+    // &-> mengakses akses memory
+    
+    // int a =10;
+    // int *b = &a;
+    // cout << "nilai a adlaha = "<< a;
+    // cout << "almaat a" << &a << endl;
+    // cout << "alaamt = "<< b;
+
+
+
+    // int b;
+    // int a = 10;
+    // b = a;
+    // cout << "alamat b" << &b << endl;
+    // cout << "alamat a" << &a << endl;
+    // cout << "value a" << a << endl;
+    // cout << "value b" << b << endl;
+
+
+    // // pass by reference
+    // int c = 10;
+    // int *d = &c;
+    // cout << "valie c " << c << endl;
+    // cout << "valie d " << *d << endl;
+    // cout << "memo c " << &c << endl;
+    // cout << "memo d " << d << endl;
+
+    
+
+   
+
+
+    return 0;
+}
+
